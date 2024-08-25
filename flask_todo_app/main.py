727a7132381f2be7be2b47013b336bb0ffb82966
GiddyFia <gideonl486@gmail.com>
2024-08-24 20:59:29 -0700

    first commit

---
+++ flask_todo_app/main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+# A list to hold the tasks
+tasks = []
+
+@app.route('/')
+def home():
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/add', methods=['POST'])
+def add_task():
+    task = request.form.get('task')
+    if task:
+        tasks.append(task)
+    return redirect(url_for('home'))
+
+@app.route('/delete/<int:task_index>')
+def delete_task(task_index):
+    if 0 <= task_index < len(tasks):
+        tasks.pop(task_index)
+    return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
